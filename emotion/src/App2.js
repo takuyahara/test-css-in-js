a08ceb9387abbd3a246aabb9fa014889a0910f22
takuyahara <h.taku86@gmail.com>
2023-04-15 16:17:28 +0000

    Added another component

---
+++ emotion/src/App2.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const StyledDiv = styled.div`
+  margin: 32px;
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  &:hover {
+    color: white;
+  }
+`;
+const StyledDiv2 = styled.div`
+  margin: 32px;
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  &:hover {
+    color: white;
+  }
+`;
+const GenStyledDiv = styled.div`
+  margin: 32px;
+  padding: 32px;
+  background-color: ${(props) => props.color};
+  font-size: 24px;
+  border-radius: 4px;
+  &:hover {
+    color: white;
+  }
+`;
+
+export default function App2() {
+  return (
+    <>
+      <StyledDiv>&lt;StyledDiv&gt;</StyledDiv>
+      <StyledDiv>&lt;StyledDiv&gt;</StyledDiv>
+      <StyledDiv2>&lt;StyledDiv2&gt;</StyledDiv2>
+      <GenStyledDiv color="hotpink">
+        &lt;GenStyledDiv color=&quot;hotpink&quot;&gt;
+      </GenStyledDiv>
+      <GenStyledDiv color="hotpink">
+        &lt;GenStyledDiv color=&quot;hotpink&quot;&gt;
+      </GenStyledDiv>
+    </>
+  );
+}
--- emotion/src/index.js
+++ emotion/src/index.js
@@ -1,13 +1,16 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App';
+import App from "./App";
+import App2 from "./App2";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <App />
+    <App />
+    <App2 />
   </StrictMode>
 );
