69e1a95ddc27359cab158e816c02be3982412239
takuyahara <h.taku86@gmail.com>
2023-04-16 17:14:46 +0000

    Add: Tailwind@2.0.4

---
+++ tailwind/src/App.tsx
@@ -0,0 +1,21 @@
+import "./input.css";
+
+const style1 = `m-8 p-8 bg-pink-600 text-2xl rounded hover:text-white`;
+const style2 = `m-8 p-8 bg-pink-600 text-2xl rounded hover:text-white`;
+const genStyle = (bgcolor: string) =>
+  `m-8 p-8 ${bgcolor} text-2xl rounded hover:text-white`;
+export default function App() {
+  return (
+    <>
+      <div className={style1}>class=&quot;{style1}&quot;</div>
+      <div className={style2}>class=&quot;{style2}&quot;</div>
+      <div className={style2}>class=&quot;{style2}&quot;</div>
+      <div className={genStyle("bg-pink-600")}>
+        class=&quot;{genStyle("bg-pink-600")}&quot;
+      </div>
+      <div className={genStyle("bg-pink-600")}>
+        class=&quot;{genStyle("bg-pink-600")}&quot;
+      </div>
+    </>
+  );
+}
